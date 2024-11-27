18c1097ebe4106c2c33699c0971049e3ad5c654c
AfrinSultana-Engineer <afrin12124@gmail.com>
2024-11-27 10:10:37 +0600

    Add the CRUD System and Paginate

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome', ['posts'=>Post::paginate(3)]);
+})->name('home');
+
+Route::get('/create', [PostController::class, 'create']);
+Route::post('/store',[PostController::class, 'ourfilestore'])->name('store');
+Route::get('/edit/{id}', [PostController::class, 'editData'])->name('edit');
+Route::post('/update/{id}', [PostController::class, 'updateData'])->name('update');
+Route::get('/delete/{id}', [PostController::class, 'deleteData'])->name('delete');
